c0518fe9c551fc311ca1f19e17e16a12d4d931ad
jakebrowning90 <jacobbrowning90@gmail.com>
2023-03-22 13:28:56 -0400

    Add single input form submission logic

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,7 +1,6 @@
 //Contains logic and manages state
 
 import React, { Component } from "react";
-import Overview from "./components/Overview";
 import './styles/styles.css';
 //Package to create unique ids as object keys
 import uniqid from "uniqid";
@@ -12,9 +11,55 @@ import ExperienceOutput from "./components/ExperienceOutput";
 class App extends Component {
   constructor() {
     super();
+
+    // Defines state
+    this.state = {
+      // Handles what's typed in the input field
+      personal: { 
+        name: '',
+        phone: '',
+        email: '',
+        website: '',
+        id: uniqid(),
+      },
+      personalCurrent: { 
+        name: '',
+        phone: '',
+        email: '',
+        website: '',
+        id: uniqid(),
+      },
+    };
   }
 
+  handleChange = (e) => {
+    this.setState({
+      personal : {
+        name: e.target.value,
+      }
+    });
+  };
+
+  // onSubmit handler for form
+  onSubmitName = (e) => {
+    //Prevent default behavior (form refresh)
+    e.preventDefault();
+    // Modify state
+    this.setState({
+      // Add current task (from input field) to the tasks array
+      personalCurrent : this.state.personal,
+      // sets task in state to an empty string to clear input field
+      personal: { 
+        name: '',
+        id: uniqid(),
+      },
+    });
+  };
+
   render() {
+    // Destructures state for cleaner look
+    const { personal, personalCurrent } = this.state;
+
     return (
       <div>
         <h1>CV Builder</h1>
@@ -22,9 +67,11 @@ class App extends Component {
           <div className="bodyForms">
              <h2>Add Personal info:</h2>
               {/* Form for Personal info input */}
-             <form className="personalEntryForm">
-              <label htmlFor= "nameInput">Name: 
+             <form className="personalEntryForm" onSubmit={this.onSubmitName}>
+              <label htmlFor= "nameInput" >Name: 
                 <input 
+                    onChange={this.handleChange}
+                    value={personal.name}
                     type="text" 
                     id="nameInput"
                 />
@@ -50,7 +97,7 @@ class App extends Component {
                 />
               </label>
 
-              <button>Save</button>
+              <button>Update</button>
             </form>
 
             <h2>Add Education:</h2>
@@ -136,7 +183,7 @@ class App extends Component {
 
           <div className="bodyOutput">
             <h2>Personal info</h2>
-            <PersonalOutput />
+            <PersonalOutput personalCurrent={personalCurrent} />
             <h2>Education</h2>
             <EducationOutput />
             <h2>Experience</h2>
--- cv-project/src/components/PersonalOutput.js
+++ cv-project/src/components/PersonalOutput.js
@@ -4,12 +4,18 @@ import React from "react";
 
 const PersonalOutput = (props) => {
     // Gets tasks array from props
-    const { tasks } = props;
+    const { personalCurrent } = props;
 
     // For each task in array, creates an <li>
     return (
         <div className="personalOutputDiv">
-            Personal info goes here
+            {/* Personal info goes here */}
+            <ul>
+                <li>{personalCurrent.name}</li>
+                <li>{personalCurrent.phone}</li>
+                <li>{personalCurrent.email}</li>
+                <li>{personalCurrent.website}</li>
+            </ul>
         </div>
     );
 };
